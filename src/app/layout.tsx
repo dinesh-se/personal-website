949264f83c42eb7b702890842d72ae89e859889b
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2024-03-09 09:57:23 +0000

    feat(analytics): added vercel analytics to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 
 import { Footer } from '@components/Footer';
@@ -28,6 +29,7 @@ export default function RootLayout({
 					{children}
 				</main>
 				<Footer></Footer>
+				<Analytics />
 			</body>
 		</html>
 	);
