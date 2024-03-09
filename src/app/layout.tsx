5f512e61fe2fc14822046c8c9b8f02a1f2b80f17
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2024-03-09 11:49:56 +0000

    feat(insights): added page insights for performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 
 import { Footer } from '@components/Footer';
@@ -29,6 +30,7 @@ export default function RootLayout({
 					{children}
 				</main>
 				<Footer></Footer>
+				<SpeedInsights />
 				<Analytics />
 			</body>
 		</html>
