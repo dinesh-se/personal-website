13d2752dc860de07e664a090fea643dae993e075
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2024-03-10 20:53:20 +0000

    chore(perf insights): removed vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 
 import { Footer } from '@components/Footer';
@@ -30,7 +29,6 @@ export default function RootLayout({
 					{children}
 				</main>
 				<Footer></Footer>
-				<SpeedInsights />
 				<Analytics />
 			</body>
 		</html>
