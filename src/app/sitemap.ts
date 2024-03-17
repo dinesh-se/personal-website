9d9ae4d04aa374c20cd87911fd0a22d5a3c077c8
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2024-03-17 16:33:48 +0000

    chore(seo): added sitemap and robots files

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+
+const sitemap = (): MetadataRoute.Sitemap => {
+	const routes: string[] = ['', '/about', '/projects', '/uses'];
+
+	return routes.map((path) => ({
+		url: `https://dineshharibabu.in${path}`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: path ? 0.8 : 1,
+	}));
+};
+
+export default sitemap;
