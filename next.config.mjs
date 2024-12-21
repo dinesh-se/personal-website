7f629871fb4db6123a76373366008161904d597c
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2024-12-21 16:52:11 +0000

    chore(build): turbo only for dev

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,13 @@
 
 const nextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		turbo: {
-			rules: {
-				'*.svg': {
-					loaders: ['@svgr/webpack'],
-					as: '*.js',
-				},
-			},
-		},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
 	},
 	images: {
 		remotePatterns: [
